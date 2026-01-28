e851f59e96a5dd49fbf12623d9d88a8942b4c568
PratikParate <paratepratik10@gmail.com>
2026-01-28 22:03:44 +0530

    replace logo with logo2 in assets and update imports

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,7 +3,7 @@ import gallery_icon from './gallery_icon.svg'
 import help_icon from './help_icon.png'
 import logo_icon from './logo_icon.svg'
 import logo_big from './logo_big.svg'
-import logo from './logo.png'
+import logo2 from './logo2.png'
 import profile_richard from './profile_richard.png'
 import profile_alison from './profile_alison.png'
 import profile_enrique from './profile_enrique.png'
@@ -28,7 +28,7 @@ const assets = {
     help_icon,
     logo_big,
     logo_icon,
-    logo,
+    logo2,
     search_icon,
     send_button,
     menu_icon,
