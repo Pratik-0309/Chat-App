2d230968c653af9fc57936e559f19eadd59cc2b4
PratikParate <paratepratik10@gmail.com>
2026-01-28 22:05:47 +0530

    implement routing with Home, Login, and Profile pages in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="min-h-screen w-full bg-black bg-[url('./src/assets/bgImage.svg')] bg-no-repeat bg-contain bg-center">
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/Profile' element={<Profile/>} />
+      </Routes>
+    </div>
   )
 }
 
