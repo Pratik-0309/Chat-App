906e8898ae77f32e82d6fdb84ed709fcfc827cbe
PratikParate <paratepratik10@gmail.com>
2026-01-31 10:28:25 +0530

    add authentication middleware to verify user tokens

---
+++ backend/middleware/verifyAuth.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const verifyAuth = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decodedToken) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    const user = await User.findById(decodedToken._id).select(
+      "-password -refreshToken",
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Authentication error:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+
+export default verifyAuth;
