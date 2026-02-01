8bc7e77a50407aebb26607c3928d7430c6c12958
PratikParate <paratepratik10@gmail.com>
2026-02-01 23:38:10 +0530

    add Toaster component for toast notifications in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
     <div className="min-h-screen w-full bg-black bg-[url('./src/assets/bgImage.svg')] bg-no-repeat bg-contain bg-center">
+      <Toaster position="top-right" toastOptions={{duration: 3000}} />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
