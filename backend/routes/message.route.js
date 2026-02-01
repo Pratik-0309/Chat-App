2275ebc155a63113152b7c9de1381626cef0e5cb
PratikParate <paratepratik10@gmail.com>
2026-02-01 18:58:34 +0530

    add send message route to message router

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getAllUsers, getMessages, markMessagesAsSeen} from "../controller/message.controller.js";
+import {getAllUsers, getMessages, markMessagesAsSeen, sendMessage} from "../controller/message.controller.js";
 import verifyAuth from "../middleware/verifyAuth.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/users", verifyAuth, getAllUsers);
 router.get("/:id", verifyAuth, getMessages);
 router.put("/seen/:id", verifyAuth, markMessagesAsSeen);
+router.post("/send/:id", verifyAuth, sendMessage);
 
 export default router;
