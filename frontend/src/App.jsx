8fbf9608d354f2f6d0b5494115091361fed0a702
PratikParate <paratepratik10@gmail.com>
2026-02-02 19:24:57 +0530

    implement user authentication checks for routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import {Route, Routes} from 'react-router-dom'
+import {Navigate,Route, Routes} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import {Toaster} from 'react-hot-toast'
 
+import { useContext } from 'react'
+import { AuthContext } from './context/AuthContext.jsx'
+
 const App = () => {
+  const {user } = useContext(AuthContext);
   return (
     <div className="min-h-screen w-full bg-black bg-[url('./src/assets/bgImage.svg')] bg-no-repeat bg-contain bg-center">
       <Toaster position="top-right" toastOptions={{duration: 3000}} />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/Profile' element={<Profile/>} />
+        <Route path='/' element={user ? <Home/> : <Navigate to="/login" />} />
+        <Route path='/login' element={!user ? <Login/> : <Navigate to="/" />} />
+        <Route path='/profile' element={user ? <Profile/> : <Navigate to="/login" />} />
       </Routes>
     </div>
   )
