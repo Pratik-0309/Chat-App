29c74c252adbed0d37be3073ffb9727fb7f5d769
PratikParate <paratepratik10@gmail.com>
2026-02-03 22:29:22 +0530

    implement logout functionality in Sidebar component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import assets, { userDummyData } from '../assets/assets.js'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext.jsx';
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
 
+  const {logout} = useContext(AuthContext)
   const navigate = useNavigate();
 
   return (
@@ -17,7 +20,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
           border-gray-600 text-gray-100 hidden group-hover:block'>
             <p onClick={()=> navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
             <hr className="my-2 border-t border-gray-500"/>
-            <p className='cursor-pointer text-sm'>Logout</p>
+            <p onClick={()=> logout()} className='cursor-pointer text-sm'>Logout</p>
           </div>
           </div>
         </div>
