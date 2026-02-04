ee7ce13160afd1f8bda2df97953cb47740fb99c1
PratikParate <paratepratik10@gmail.com>
2026-02-04 23:21:47 +0530

    add image upload support to sendMessage route

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import {getAllUsers, getMessages, markMessagesAsSeen, sendMessage} from "../controller/message.controller.js";
 import verifyAuth from "../middleware/verifyAuth.js";
+import upload from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.get("/users", verifyAuth, getAllUsers);
 router.get("/:id", verifyAuth, getMessages);
 router.put("/seen/:id", verifyAuth, markMessagesAsSeen);
-router.post("/send/:id", verifyAuth, sendMessage);
+router.post("/send/:id", verifyAuth,upload.single("image"), sendMessage);
 
 export default router;
